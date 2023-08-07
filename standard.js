57da6735391ea430258fe1f6439889af304a6cef
Jed G <43355574+JedGascho@users.noreply.github.com>
2023-08-07 16:13:39 -0400

    Update standard.js

--- standard.js
+++ standard.js
@@ -2,8 +2,10 @@ const audio = new Audio('tick2.wav');
 var tmpo = 0;
 var i = 1;
 var p = true;
+var is_playing = null;
 function myLoop() {
     //console.log(parseInt(rangeValue.innerText));
+    is_playing = true
     setTimeout(function() {
         if (p == true){
             audio.play()
@@ -19,13 +21,25 @@ function play_sound(tempo){
     tmpo = tempo;
     //document.getElementById('tempo').innerHTML = tmpo/100*6
     p = true
-    audio.play()
-    myLoop(tmpo)
+    if (is_playing != true){
+        audio.play()
+    myLoop()
+    }
     
 }
 
 function stop_sound(){
     p = false;
-    myLoop()
+    is_playing = false
+    //myLoop()
 }
       
+function stop_start(){
+    if (is_playing == true){
+        stop_sound()
+        document.getElementById("stop_start_btn").innerHTML = "⏵";
+    } else {
+        play_sound()
+        document.getElementById("stop_start_btn").innerHTML = "⏸";
+    }
+}
